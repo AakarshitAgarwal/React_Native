c56547533e3139d2e287d25bee57ece9943ddb7a
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-23 03:07:04 +0530

    Adding Screens

--- navigation/screens/SplashScreen.js
+++ navigation/screens/SplashScreen.js
@@ -1,18 +1,75 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { 
+    View, 
+    Text, 
+    TouchableOpacity, 
+    Dimensions,
+    StyleSheet,
+    Image,
+    Button
+} from 'react-native';
 
-export default class SplashScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <View>
-        <Text> SplashScreen </Text>
-      </View>
+const SplashScreen =()=>{
+    return(
+        <View style={styles.container}>
+            <Button
+            title ="Click Here"
+            onPress={()=>alert('Button Clicked:')}
+            />
+        </View>
     );
+};
+
+export default SplashScreen;
+
+const {height} = Dimensions.get("screen");
+const height_logo = height * 0.28;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#009387'
+  },
+  header: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  footer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      paddingVertical: 50,
+      paddingHorizontal: 30
+  },
+  logo: {
+      width: height_logo,
+      height: height_logo
+  },
+  title: {
+      color: '#05375a',
+      fontSize: 30,
+      fontWeight: 'bold'
+  },
+  text: {
+      color: 'grey',
+      marginTop:5
+  },
+  button: {
+      alignItems: 'flex-end',
+      marginTop: 30
+  },
+  signIn: {
+      width: 150,
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 50,
+      flexDirection: 'row'
+  },
+  textSign: {
+      color: 'white',
+      fontWeight: 'bold'
   }
-}
+});
