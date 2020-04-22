986eb00a81ced3be5820c776551e16f246822611
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-23 02:53:59 +0530

    Reworking_navigation

--- navigation/App.js
+++ navigation/App.js
@@ -1,46 +1,39 @@
-import React, { Component } from 'react';
-import {
-  View, 
-  Text,
-  Button
-  } from 'react-native';
-  import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// import { DrawerContent } from './screens/DrawerContent';
+
+// import MainTabScreen from './screens/MainTabScreen';
+// import SupportScreen from './screens/SupportScreen';
+// import SettingsScreen from './screens/SettingsScreen';
+// import BookmarkScreen from './screens/BookmarkScreen';
+
+import RootStackScreen from './screens/RootStackScreen';
+
+//const Drawer = createDrawerNavigator();
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-    render() {
-    return (
-      <View>
-        <Text>This is App Component </Text>
-        <Button onPress={()=>this.props.navigation.navigate('test')} title="Go to Test"></Button>
-      </View>
-    );
-  }                     //props.navigation.navigate('KEEEYYY')
+const App = () => {
+  return (
+    <NavigationContainer>
+      <RootStackScreen/>
+      {/* <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
+      </Drawer.Navigator> */}
+    </NavigationContainer>
+  );
 }
 
-class Test extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-    render() {
-    return (
-      <View>
-        <Text>This is Test Component </Text>
-         <Button onPress={()=>this.props.navigation.navigate('home')} title="Go to App"></Button>
-      </View>
-    );
-  }
-}
-
-export default createStackNavigator({
-  home: App,
-  test: Test
-})
+export default App;
